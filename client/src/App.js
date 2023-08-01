9e2673f0f8e101d8651aa1a20ee8b20564c90b4e
fatimabatooll <fatima.batool@xloopdigital.com>
2023-08-01 14:48:36 +0500

    clear data

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, {useEffect} from 'react';
+import { AppBar, Container, Grid, Grow, Typography } from '@mui/material';
+import memories from './images/memories.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import useStyles from './styles'; 
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts'
+
+// test commitfsddsfsfdsffd
+const App = () => {
+  const classes = useStyles(); 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+  return (
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'> {/* Apply the 'appBar' class */}
+        <Typography className={classes.heading} variant='h2' align='center'>Memories  </Typography>
+        <img src={memories} alt="memories" height="60"/>      
+      </AppBar>
+      <Grow in>
+          <Container>
+            <Grid  container justify="space-between" alignItems="stretch" spacing={3}> {/* Fix the 'alighnItems' to 'alignItems' */}
+               <Grid item xs={12} sm={7}>
+                 <Posts/>
+               </Grid>
+               <Grid item xs={12} sm={4}>
+                  <Form/>
+               </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+    </Container>
+  )
+}
+
+export default App;
+
+// import React from 'react';
+// import Button from '@mui/material/Button';
+// import useStyles from './styles';
+
+// const App = () => {
+//   const classes = useStyles();
+
+//   return (
+//     <Button className={classes.button} variant="contained" color="primary">
+//       Hello
+//     </Button>
+//   );
+// };
+
+// export default App;
---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+const theme = createTheme({
+
+});
+
+const store = configureStore({
+  reducer: rootReducer, // Pass the rootReducer as the "reducer" property
+}, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+);
